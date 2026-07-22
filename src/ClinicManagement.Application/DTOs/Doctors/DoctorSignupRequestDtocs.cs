9aa62604330b8dec8903ad747765b0e736d5efe5
HP <HP@LAPTOP-GORF2L20>
2026-07-22 18:42:15 +0330

    Doctor Signup

---
+++ src/ClinicManagement.Application/DTOs/Doctors/DoctorSignupRequestDtocs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Application.DTOs.Doctors
+{
+ public  record DoctorSignupRequestDtocs
+    {
+        public required string MedicalID { get; set; }
+        public required string Name { get; set; }
+        public string Password { get; set; }
+        public decimal Fee { get; set; }
+
+
+    }
+}
