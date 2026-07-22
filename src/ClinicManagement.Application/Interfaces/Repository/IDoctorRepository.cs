7f3c35dcab478967bac1043343c1cdbb7c1d4814
HP <HP@LAPTOP-GORF2L20>
2026-07-22 18:48:38 +0330

    Task3

---
+++ src/ClinicManagement.Application/Interfaces/Repository/IDoctorRepository.cs
@@ -0,0 +1,19 @@
+﻿using ClinicManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Application.Interfaces.Repository
+{
+    public interface IDoctorRepository
+    {
+
+        public string GetByMedicalIdAsync(string medicallId);
+
+        public bool ExistsByMedicalIdAsync(string medicalIdl);
+
+        public void AddAsync(Doctor doctor);
+
+
+    }
+}
