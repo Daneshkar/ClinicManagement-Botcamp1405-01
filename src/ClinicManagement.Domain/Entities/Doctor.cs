a27008eae81ba0fef4e49b001fbeadbd54b572f0
HP <HP@LAPTOP-GORF2L20>
2026-07-22 18:34:42 +0330

    task1:entity doctor

---
+++ src/ClinicManagement.Domain/Entities/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Domain.Entities
+{
+    public class Doctor
+
+    {
+        public required string Name { get; set; }
+        public required string MedicalID { get; set; }
+        public string PasswordHash { get; set; }
+        public decimal Fee { get; set; }
+    }
+
+
+}
